ab29a6a57a314f6e72b5eec1fc0fdbfdb36d15c3
amirshamia <amir0988@gmail.com>
2023-09-27 16:18:04 +0300

    added _ to scss files

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,12 +1,34 @@
 
 
-export function AppFooter(){
-    
+export function AppFooter() {
+
 
     return (
         <section className="footer-container">
+            <main>
+                <section className="title">
+                    <div>myteam</div>
+                    <div>
 
-            
+                    <span>home</span>
+                    <span>about</span>
+                    </div>
+                </section>
+                <section>
+                    987  Hillcrest LaneIrvine, CACalifornia 92714Call Us : 949-833-7432
+                </section>
+                <section>
+                    <img src="./src/assets/PP-images/icon-facebook.svg" alt="" />
+                    <img src="./src/assets/PP-images/icon-pinterest.svg" alt="" />
+                    <img src="./src/assets/PP-images/icon-twitter.svg" alt="" />
+
+
+                    <span>
+                        Copyright 2020. All Rights Reserved
+                    </span>
+                </section>
+            </main>
+
         </section>
     )
 }
